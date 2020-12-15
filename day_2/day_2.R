0149f93464c961da4edc76924d74a92244971c0b
Tyler Mallon <tylerm@BEL-MBP-01.local>
2020-12-14 20:17:56 -0800

    part 1 complete, part 2 unfinished

--- day_2/day_2.R
+++ day_2/day_2.R
@@ -1,11 +1,34 @@
 # Part 1
 library(tidyverse)
 
-input <- read_tsv("day_2/input.txt", col_names = FALSE)
+input <- read_lines("day_2/input.txt")
 
-input %>%
-  mutate(lower_bound = str_extract(),
-         upper_bound = str_extract(),
-         rule = str_extract(),
-         code = str_extract(),
-         valid = str_detect()) 
+passwords <- str_match(input, "([0-9]+)-([0-9]+)\\s([a-z]+):\\s([a-z]+)") %>%
+  as_tibble() %>%
+  rename(pw_code = "V1",
+         pw_input1 = "V2",
+         pw_input2 = "V3",
+         pw_char = "V4",
+         pw = "V5") %>%
+  mutate(pw_input1 = as.numeric(pw_input1),
+         pw_input2 = as.numeric(pw_input2))
+
+passwords %>%
+  mutate(count = map2_int(.x = pw, 
+                      .y = pw_char,
+                      .f = ~ str_count(.x, .y)
+                      ),
+         valid = if_else(count >= pw_input1 & count <= pw_input2,
+                   "Valid",
+                   "Invalid")) %>%
+  group_by(valid) %>%
+  count()
+
+# Part 2
+
+test <- passwords %>%
+  mutate(possible_positions = map2(.x = pw,
+                                   .y = pw_char,
+                                   .f = ~str_locate_all(.x, .y))) %>%
+  unnest(possible_positions)
+  
